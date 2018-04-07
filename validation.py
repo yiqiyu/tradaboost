b58db94c9cff5f709364b96face3a6c1dc77b6ce
Dante Yu <dante3@126.com>
2018-04-07 12:04:01 +0800

    fix

--- validation.py
+++ validation.py
@@ -2,9 +2,10 @@ import time
 import numbers
 import warnings
 
+import numpy as np
 from sklearn.model_selection._validation import _index_param_value, _safe_split, _score, FitFailedWarning, logger, _num_samples
 import sklearn.model_selection._validation
-from sklearn.model_selection._validation import cross_val_score
+from sklearn.model_selection._validation import cross_val_score, cross_val_predict
 
 
 def _fit_and_score(estimator, X, y, scorer, train, test, verbose,
@@ -178,4 +179,72 @@ def _fit_and_score(estimator, X, y, scorer, train, test, verbose,
     return ret
 
 
+def _fit_and_predict(estimator, X, y, train, test, verbose, fit_params,
+                     method):
+    """Fit estimator and predict values for a given dataset split.
+
+    Read more in the :ref:`User Guide <cross_validation>`.
+
+    Parameters
+    ----------
+    estimator : estimator object implementing 'fit' and 'predict'
+        The object to use to fit the data.
+
+    X : array-like of shape at least 2D
+        The data to fit.
+
+    y : array-like, optional, default: None
+        The target variable to try to predict in the case of
+        supervised learning.
+
+    train : array-like, shape (n_train_samples,)
+        Indices of training samples.
+
+    test : array-like, shape (n_test_samples,)
+        Indices of test samples.
+
+    verbose : integer
+        The verbosity level.
+
+    fit_params : dict or None
+        Parameters that will be passed to ``estimator.fit``.
+
+    method : string
+        Invokes the passed method name of the passed estimator.
+
+    Returns
+    -------
+    predictions : sequence
+        Result of calling 'estimator.method'
+
+    test : array-like
+        This is the value of the test parameter
+    """
+    # Adjust length of sample weights
+    fit_params = fit_params if fit_params is not None else {}
+    fit_params = dict([(k, _index_param_value(X, v, train))
+                      for k, v in fit_params.items()])
+    fit_params["indices"] = train                   # the different part
+
+    X_train, y_train = _safe_split(estimator, X, y, train)
+    X_test, _ = _safe_split(estimator, X, y, test, train)
+
+    if y_train is None:
+        estimator.fit(X_train, **fit_params)
+    else:
+        estimator.fit(X_train, y_train, **fit_params)
+    func = getattr(estimator, method)
+    predictions = func(X_test)
+    if method in ['decision_function', 'predict_proba', 'predict_log_proba']:
+        n_classes = len(set(y))
+        predictions_ = np.zeros((X_test.shape[0], n_classes))
+        if method == 'decision_function' and len(estimator.classes_) == 2:
+            predictions_[:, estimator.classes_[-1]] = predictions
+        else:
+            predictions_[:, estimator.classes_] = predictions
+        predictions = predictions_
+    return predictions, test
+
+
 sklearn.model_selection._validation._fit_and_score = _fit_and_score
+sklearn.model_selection._validation._fit_and_predict = _fit_and_predict
